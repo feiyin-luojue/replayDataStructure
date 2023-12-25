ec0ef4784fa744ea670c0f8b7c983390c243387e
fei <1060352230@qq.com>
2023-12-25 15:25:26 +0800

    [修改] 完成二叉搜索时的初始化API接口

--- BinarySearchTree/binarySearchTree.c
+++ BinarySearchTree/binarySearchTree.c
@@ -1,12 +1,43 @@
 #include "binarySearchTree.h"
+#include <stdlib.h>
 
+/* 状态码 */
+enum STATUS_CODE
+{
+    NOT_FIND = -1,
+    ON_SUCCESS,
+    NULL_PTR,
+    MALLOC_ERROR,
+    INVALID_ACCESS,
+};
 
 /* 二叉搜索树的初始化 */
 int binarySearchTreeInit(BinarySearchTree **pBstree)
 {
     int ret = 0;
+    BinarySearchTree * bstree = (BinarySearchTree *)malloc(sizeof(BinarySearchTree) * 1);
+    if (bstree == NULL)
+    {
+        return NULL_PTR;
+    }
+    memset(bstree, 0, sizeof(BinarySearchTree) * 1);
+    /* 初始化树 */
+    bstree->root = NULL;
+    bstree->size = 0;
 
+    bstree->root = (BSTreeNode *)malloc(sizeof(BSTreeNode) * 1);
+    if (bstree->root == NULL)
+    {
+        return NULL_PTR;
+    }
+    memset(bstree->root, 0, sizeof(BSTreeNode) * 1);
 
+    bstree->root->data = 0;
+    bstree->root->parent = NULL;
+    bstree->root->left = NULL;
+    bstree->root->right = NULL;
+
+    *pBstree = bstree;
     return ret;
 }
 
