88884bad563bc9973460201fdbbc4eec9732137d
fei <1060352230@qq.com>
2023-12-29 14:34:07 +0800

    [优化] 合并左、右旋API接口

--- BalanceBinarySearchTree/balanceBinarySearchTree.c
+++ BalanceBinarySearchTree/balanceBinarySearchTree.c
@@ -386,6 +386,40 @@ static int AVLTreeCurrentNodeRotateRight(BalanceBinarySearchTree *pBstree, AVLTr
     return ON_SUCCESS;
 }
 
+/* 旋转 */
+static int AVLTreeCurrentNodeRotate(BalanceBinarySearchTree *pBstree, AVLTreeNode *grand, AVLTreeNode *parent, AVLTreeNode *child)
+{
+    grand->left = child;                    //1 
+    parent->right = grand;                  //2
+
+    /* p成为新的根结点 */
+    parent->parent = grand->parent;         //3
+
+    if (AVLTreeCurrentNodeIsLeft(grand))
+    {
+        grand->parent->left = parent;       //4
+    }
+    else if (AVLTreeCurrentNodeIsRight(grand))
+    {
+        grand->parent->right = parent;      //4
+    }
+    else 
+    {
+        /* p 成为树的根节点 */
+        pBstree->root = parent;             //4
+    }
+    grand->parent = parent;                 //5
+
+    if (child != NULL)
+    {
+        child->parent = grand;              //6
+    }
+
+    /* 更新高度 */
+    AVLTreeNodeUpdateHeight(grand);
+    AVLTreeNodeUpdateHeight(parent);
+    return ON_SUCCESS;
+}
 
 /* 调整平衡 */
 /* node一定是最低的不平衡结点 */
@@ -400,7 +434,11 @@ static int AVLTreeNodeAdjustBalance(BalanceBinarySearchTree *pBstree,AVLTreeNode
         if (AVLTreeCurrentNodeIsLeft(child))
         {
             /* LL */  
+            #if 0
             AVLTreeCurrentNodeRotateRight(pBstree, node);
+            #else
+            AVLTreeCurrentNodeRotate(pBstree, node, parent, child);
+            #endif
         }
         else if (AVLTreeCurrentNodeIsRight(child))
         {
@@ -421,7 +459,11 @@ static int AVLTreeNodeAdjustBalance(BalanceBinarySearchTree *pBstree,AVLTreeNode
         else if (AVLTreeCurrentNodeIsRight(child))
         {
             /* RR */
+            #if 0
             AVLTreeCurrentNodeRotateLeft(pBstree, node);
+            #else
+            AVLTreeCurrentNodeRotate(pBstree, node, parent, child);
+            #endif
         }
     }
 }
