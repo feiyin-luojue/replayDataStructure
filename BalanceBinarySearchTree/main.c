2fc06436103d0ad4f4d72533bbd7efc5c9062d41
fei <1060352230@qq.com>
2024-01-18 21:07:13 +0800

    [修改] 完成二叉搜索s树的测试代码

--- BalanceBinarySearchTree/main.c
+++ BalanceBinarySearchTree/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "balanceBinarySearchTree.h"
 
 #define BUFFER_SIZE 10
 
 /* 测试二叉搜索树 */
+
 int compareBasicDataFunc(void * arg1, void *arg2)
 {
     int val1 = *(int *)arg1;
@@ -24,11 +26,10 @@ int printBasicData(void *arg)
 
 int main(int argc, char const *argv[])
 {
-    /* */
-    BalanceBinarySearchTree * BST = NULL;
+    balanceBinarySearchTree * BST = NULL;
     balanceBinarySearchTreeInit(&BST, compareBasicDataFunc, printBasicData);
 
-    int buffer[BUFFER_SIZE] = {56, 28, 75, 73, 77, 13, 7, 26, 100, 12};
+    int buffer[BUFFER_SIZE] = {13, 14, 15, 12, 11, 17, 16, 8, 9, 1};
 
     for (int idx = 0; idx < BUFFER_SIZE; idx++)
     {
@@ -37,19 +38,30 @@ int main(int argc, char const *argv[])
     /* 获取二叉搜索树的结点个数 */
     int size = 0;
     balanceBinarySearchTreeGetNodeSize(BST, &size);
-    printf("size:%d\n", size);
+    printf("结点个数size:%d\n", size);
     /* 获取二叉搜索树的高度 */
     int heigh = 0;
     balanceBinarySearchTreeGetHeight(BST, &heigh);
-    printf("heigh:%d\n", heigh);
+    printf("树高heigh:%d\n", heigh);
 
-    // /* 中序遍历 */
-    // binarySearchTreeinOrderTravel(BST);
-    // putchar(10);
+    /* 中序遍历 */
+    printf("中序遍历： ");
+    balanceBinarySearchTreeInOrderTravel(BST);
+    putchar(10);
 
-    // /* 层序遍历 */
-    // binarySearchTreeLevelOrderTravel(BST);
-    // putchar(10);
+    /* 层序遍历 */
+    printf("层序遍历： ");
+    balanceBinarySearchTreeLevelOrderTravel(BST);
+    putchar(10);
+
+    /* 后序遍历 */
+    printf("后序遍历： ");
+    balanceBinarySearchTreePostOrderTravel(BST);
+    putchar(10);
+
+    /* 销毁树 */
+    balanceBinarySearchTreeDestroy(BST);
+
 
 
 
